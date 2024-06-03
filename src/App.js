eb5299f8f9d397a1eb6e48ddd446553b2a026ec6
unknown <keerthimurgan@gmail.com>
2024-06-03 13:11:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout';
+import AddPostsForm from './features/posts/AddPostsForm';
+import PostsList from './features/posts/PostsList';
+// import fp from './fp';
+import { Routes,Route, Navigate } from 'react-router-dom';
+import SinglePostPage from './features/posts/SinglePostPage';
+import EditPostForm from './features/posts/EditPostForm';
+import UsersList from './features/users/UsersList';
+import UserPage from './features/users/UserPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <main>
+            {/* <fp /> */}
+            {/* <Counter/> */}
+            {/* <AddPostsForm />
+            <PostsList /> */}
+            <Routes>
+                <Route path='/' element={<Layout />} >
+                <Route index element={<PostsList />} />
+                <Route path='post'>
+                    <Route index element={<AddPostsForm />} />
+                    <Route path=':postId' element={<SinglePostPage />} />
+                    <Route path='edit/:postId' element={<EditPostForm />} />
+                </Route>
+                <Route path='user'>
+                    <Route index element={<UsersList />} />
+                    <Route path=':userId' element={<UserPage />} />
+                </Route>
+                <Route path='*' element={<Navigate to ='/' replace />}/>
+                </Route>
+
+            </Routes>
+        
+        </main>
+    )
 }
 
 export default App;
